a89eb9d5f11028eb490de47ce62f6be2e65f8081
BuildTools <unconfigured@null.spigotmc.org>
2016-11-10 16:04:35 +0100

    Fixade bugg som tillät otillåtna drag

--- Spel/reversi/__init__.py
+++ Spel/reversi/__init__.py
@@ -81,7 +81,7 @@ class Board:
                 candidates.append(pos)
                 pos = (pos[0] + direction[0], pos[1] + direction[1])
 
-            if 0 <= pos[0] <= self.size-1 and 0 <= pos[1] <= self.size-1:
+            if 0 <= pos[0] <= self.size-1 and 0 <= pos[1] <= self.size-1 and self.board[pos[0]][pos[1]] == type:
                 flipped += candidates
 
         return flipped
