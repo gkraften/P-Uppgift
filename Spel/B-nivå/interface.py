a32d032d33736ba7203edb348bd871bc7b5f02e0
BuildTools <unconfigured@null.spigotmc.org>
2016-11-10 16:02:57 +0100

    Implementerade B-nivå

---
+++ Spel/B-nivå/interface.py
@@ -0,0 +1,87 @@
+import reversi
+import string
+import math
+
+def print_separator(width, offset):
+    '''Print a line separating two rows in the board.'''
+
+    print(" "*offset, end="")
+    print("—"*width)
+
+def digits(d):
+    '''Returns the number of digits in d.'''
+
+    return int(math.log10(d)) + 1
+
+def get_int(msg, error):
+    success = False
+    val = None
+    while not success:
+        try:
+            val = int(input(msg))
+            success = True
+        except ValueError:
+            print(error)
+
+    return val
+
+def get_row_col(size):
+    '''Returns selected row and column. size is the
+    size of the board.'''
+
+    row = get_int("Välj RAD att placera pjäs på: ", "Raden måste vara en siffra")
+    while not 1 <= row <= size:
+        print("Raden måste ligga mellan 1 och %d" % size)
+        row = get_int("Välj RAD att placera pjäs på: ", "Raden måste vara en siffra")
+
+    col_input = input("Välj KOLUMN att placera pjäs på: ").upper()
+    while len(col_input) != 1 or not (col_input in string.ascii_uppercase and string.ascii_uppercase.index(col_input) < size):
+        print("Kolumnen måste vara en bokstav mellan A och %s" % string.ascii_uppercase[size])
+        col_input = input("Välj KOLUMN att placera pjäs på: ").upper()
+
+    col = string.ascii_uppercase.index(col_input)
+
+    return (row-1, col)
+
+def should_skip():
+    '''Returns whether the user wants to skip its
+    turn or not.'''
+
+    skip = input("Vill du stå över din runda (enter för nej, vad som helst för ja)?")
+
+    return len(skip) != 0
+
+def display_board(b):
+    '''Print the board b. The width must be max 26.'''
+
+    board = b.get_board_list()
+
+    if len(board) > 26:
+        raise ValueError("Size of board must not exceed 26.")
+
+    n_digits = digits(len(board))
+    offset = n_digits + 2
+
+    print(" "*(offset+1), end="")
+
+    width = 0
+    for i in range(len(board)):
+        print(string.ascii_uppercase[i % 26], end="   ")
+        width += 4
+    width -= 1
+    print()
+
+    print_separator(width, offset)
+
+    for index,row in enumerate(board):
+        print(index+1, end=" "*(n_digits - digits(index+1)) + " | ")
+        for elem in row:
+            if elem == reversi.WHITE:
+                print("v", end="")
+            elif elem == reversi.BLACK:
+                print("s", end="")
+            else:
+                print(" ", end="")
+            print(" | ", end="")
+        print()
+        print_separator(width, offset)
---
+++ Spel/B-nivå/main.py
@@ -0,0 +1,114 @@
+# P-Uppgift - Oscar Gustavsson (ogust@kth.se)
+# E-nivå
+
+# Fult hack för att kunna importera reversi-modulerna
+# som ligger i en annan mapp
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + "..")
+
+# Här börjar själva programmet
+
+import reversi
+from reversi.highscore import Highscore
+from reversi.exceptions import *
+from reversi.bot import Bot
+import interface
+import time
+
+
+colors = {reversi.WHITE: "vit", reversi.BLACK: "svart"} # Table to translate colors into strings
+score_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + "highscore.txt" # Path to high score file. Same directory as script
+player = reversi.WHITE
+
+print(r''' _______  _______           _______  _______  _______ _________
+(  ____ )(  ____ \|\     /|(  ____ \(  ____ )(  ____ \\__   __/
+| (    )|| (    \/| )   ( || (    \/| (    )|| (    \/   ) (
+| (____)|| (__    | |   | || (__    | (____)|| (_____    | |
+|     __)|  __)   ( (   ) )|  __)   |     __)(_____  )   | |
+| (\ (   | (       \ \_/ / | (      | (\ (         ) |   | |
+| ) \ \__| (____/\  \   /  | (____/\| ) \ \__/\____) |___) (___
+|/   \__/(_______/   \_/   (_______/|/   \__/\_______)\_______/
+                          Nu med bot
+
+''')
+
+print("Välkommen till Reversi av Oscar Gustavsson!")
+print("Utmana datorn i reversi.")
+print("För att avsluta tryck ctrl+C.")
+print()
+
+if not Highscore.load(score_path):
+    print("High score kunde inte läsas in. Sätter till 0 poäng.\n")
+
+try:
+    size = interface.get_int("Skriv storleken på brädet (måste vara delbart med två och ligga mellan 4 och 26): ", "Du måste skriva ett tal!")
+    while not 4 <= size <= 26 or size % 2 == 1:
+        print("Måste vara delbart med två och ligga mellan 4 och 26!")
+        size = interface.get_int("Skriv storleken på brädet (måste vara delbart med två och ligga mellan 4 och 26): ", "Du måste skriva ett tal!")
+    print()
+
+    board = reversi.Board(size)
+    game = reversi.Reversi(board)
+    bot = Bot(game, -player)
+
+    winner = None
+    while not winner:
+        interface.display_board(board)
+
+        if game.get_turn() == player:
+            print("Din tur (%s) att spela!" % colors[player])
+
+            if interface.should_skip():
+                game.skip()
+            else:
+                success = False
+                while not success:
+                    row, col = interface.get_row_col(size)
+                    error = False
+
+                    try:
+                        game.place(row, col)
+                        success = True
+                    except NoFlipsException:
+                        print("Minst en bricka måste vändas varje omgång.")
+                        error = True
+                    except OccupiedCellException:
+                        print("Cellen är redan upptagen.")
+                        error = True
+
+                    if error and interface.should_skip():
+                        game.skip()
+                        success = True
+        else:
+            print("Datorns tur att spela!")
+
+            flips = bot.play()
+
+            if flips is None:
+                print("Datorn hoppade över sin tur.")
+            else:
+                print("Datorn har vänt %d pjäser." % len(flips))
+
+        winner = game.winner()
+
+        print("\n")
+
+    if len(winner) == 1:
+        print("Det blev lika!")
+    else:
+        win_player = "du" if winner[0] == player else "datorn"
+        print("Vinnaren är %s som vann med %d pjäser!" % (win_player, winner[1]))
+
+        if winner[1] > Highscore.get_highscore() and winner[0] == player:
+            print("Nytt high score dessutom!!")
+
+            Highscore.set_highscore(winner[1])
+
+            try:
+                Highscore.save(score_path)
+            except:
+                print("Misslyckades med att spara nya high score.")
+except KeyboardInterrupt:
+    print("\nHejdå!!")
