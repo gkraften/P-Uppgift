7e1354e9b0bd99e794f779b8d805115508893c7a
BuildTools <unconfigured@null.spigotmc.org>
2016-11-17 13:24:19 +0100

    Satte MainMenu till första scen

--- Spel/A-nivå/main.py
+++ Spel/A-nivå/main.py
@@ -12,16 +12,26 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + "..")
 
 import sfml as sf
 
+from interface.scenes import MainMenu
+
 WIDTH = 640
 HEIGHT = 480
 
 window = sf.graphics.RenderWindow(sf.window.VideoMode(WIDTH, HEIGHT), "Reversi")
 
+scene = MainMenu(window)
+
+clock = sf.system.Clock()
 while window.is_open:
     for event in window.events:
         if type(event) is sf.window.CloseEvent:
             window.close();
 
-    window.clear(sf.graphics.Color.WHITE)
+    next_scene = scene.update(clock.restart())
+
+    scene.draw()
 
     window.display()
+
+    if not next_scene is None:
+        scene = next_scene
