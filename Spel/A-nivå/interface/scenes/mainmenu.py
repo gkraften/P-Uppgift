ef0f929cde2183438ba3c82f1f518674c12a5192
BuildTools <unconfigured@null.spigotmc.org>
2016-11-17 13:20:30 +0100

    Skapade klassen MainMenu

---
+++ Spel/A-nivå/interface/scenes/mainmenu.py
@@ -0,0 +1,12 @@
+from interface import Scene
+
+import sfml as sf
+
+class MainMenu(Scene):
+    """Class representing the main menu."""
+
+    def draw(self):
+        self.target.clear(sf.graphics.Color.CYAN)
+
+    def update(self, t):
+        return None
