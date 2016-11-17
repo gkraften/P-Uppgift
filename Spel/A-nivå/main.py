3a1684c87aefbe2c3475a44e43221757a0664e10
BuildTools <unconfigured@null.spigotmc.org>
2016-11-17 13:16:11 +0100

    Implementerade  basklassen Scene

--- Spel/A-nivå/main.py
+++ Spel/A-nivå/main.py
@@ -10,3 +10,18 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + "..")
 
 # Här börjar själva programmet
 
+import sfml as sf
+
+WIDTH = 640
+HEIGHT = 480
+
+window = sf.graphics.RenderWindow(sf.window.VideoMode(WIDTH, HEIGHT), "Reversi")
+
+while window.is_open:
+    for event in window.events:
+        if type(event) is sf.window.CloseEvent:
+            window.close();
+
+    window.clear(sf.graphics.Color.WHITE)
+
+    window.display()
