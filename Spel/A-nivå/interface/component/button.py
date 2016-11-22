526f257b8adf5e402c978bebbc7b86006e30e04e
BuildTools <g.spam32@gmail.com>
2016-11-22 14:56:42 +0100

    Fixade grejer

--- Spel/A-nivå/interface/component/button.py
+++ Spel/A-nivå/interface/component/button.py
@@ -6,8 +6,6 @@ import sfml as sf
 class Button(Component):
     """A button that can be rendered to the screen."""
 
-    _MARGIN = 10
-
     def __init__(self, text, pos=(0, 0)):
         """Initialize new button with text text at
         position pos."""
@@ -16,19 +14,15 @@ class Button(Component):
         self.text = sf.graphics.Text()
         self.text.string = text
         self.text.font = font
-        self.text.character_size = 18
         self.text.color = sf.graphics.Color.WHITE
-        self.text.position = (pos[0], pos[1] + self._MARGIN)
 
-        text_bounds = self.text.local_bounds
-        self.rect = sf.graphics.RectangleShape((text_bounds.width + 2*self._MARGIN, text_bounds.height + 2*self._MARGIN))
-        self.rect.fill_color = sf.graphics.Color.RED
-        self.rect.position = pos
+        self.set_character_size(18)
+        self.set_position(pos)
 
         self.listener = None
 
     def draw(self, target):
-        target.draw(self.rect)
+        #target.draw(self.rect)
         target.draw(self.text)
 
     def set_text(self, text):
@@ -39,19 +33,18 @@ class Button(Component):
     def get_position(self):
         """Returns the buttons position."""
 
-        pos = self.rect.position
-        return (pos.x - self._MARGIN, pos.y - self._MARGIN)
+        return self.text.position
 
     def set_position(self, pos):
         """Sets the buttons position to pos."""
 
-        self.text.position = (pos[0] + self._MARGIN, pos[1] + self._MARGIN)
-        self.rect.position = pos
+        self.text.position = pos
 
     def get_bounds(self):
         """Get width and height of the button."""
 
-        return (self.text.global_bounds.width + self._MARGIN, self.text.global_bounds.height + self._MARGIN)
+        bounds = self.text.global_bounds
+        return (bounds.width, bounds.height)
 
     def set_listener(self, func):
         """Set function to execute when the button
@@ -63,7 +56,3 @@ class Button(Component):
         """Set character size."""
 
         self.text.character_size = size
-
-        text_bounds = self.text.local_bounds
-
-        self.rect.size = (text_bounds.width + 2*self._MARGIN, text_bounds.height + 2*self._MARGIN)
--- Spel/A-nivå/interface/scene/mainmenu.py
+++ Spel/A-nivå/interface/scene/mainmenu.py
@@ -8,7 +8,7 @@ class MainMenu(Scene):
     """Class representing the main menu."""
 
     TITLE_SCALE = 1/10
-    BUTTON_SCALE = 1/5
+    BUTTON_SCALE = 1/15
 
     def __init__(self, target):
         super().__init__(target)
@@ -28,15 +28,10 @@ class MainMenu(Scene):
 
         # Menu items
         self.menuitems = []
-        for item in enumerate(["Spela en spelare", "Spela två spelare", "Avsluta"]):
-            button = Button(item[1])
-            button.set_character_size(size.y * self.BUTTON_SCALE)
-
-            bounds = button.get_bounds()
-
-            button.set_position((size.x/2 - bounds[0]/2, size.y/5 + item[0]*size.y/5))
-
+        for item in ["Spela en spelare", "Spela två spelare", "Avsluta"]:
+            button = Button(item)
             self.menuitems.append(button)
+        self._setup_buttons()
 
     def draw(self):
         self.target.clear(sf.graphics.Color.BLACK)
@@ -61,3 +56,19 @@ class MainMenu(Scene):
                 self.title.character_size = int(size.y * self.TITLE_SCALE)
                 title_bounds = self.title.global_bounds
                 self.title.position = (size.x / 2 - title_bounds.width / 2, 10)
+
+                self._setup_buttons()
+
+    def _setup_buttons(self):
+        """Set the buttons positions and sizes."""
+
+        for b in enumerate(self.menuitems):
+            size = self.target.size
+
+            button = b[1]
+
+            button.set_character_size(size.y * self.BUTTON_SCALE)
+
+            bounds = button.get_bounds()
+
+            button.set_position((size.x / 2 - bounds[0] / 2, 2 * size.y / 6 + b[0] * size.y / 5))
