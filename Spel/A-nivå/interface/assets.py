8a9679c171fce4542c5c89dc3e60271873f3106d
BuildTools <g.spam32@gmail.com>
2016-11-22 13:54:17 +0100

    Implementerade assets

---
+++ Spel/A-nivå/interface/assets.py
@@ -0,0 +1,9 @@
+import os
+
+assets_path = os.path.dirname(os.path.realpath(__file__))
+
+def get_asset(path):
+    """Returns the path to an asset located in the assets
+    foldder referenced to by path."""
+
+    return assets_path + path.replace("/", os.path.sep)
