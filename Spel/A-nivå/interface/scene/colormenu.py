51460eb45f39dd22d4bccd3c94c295868b107a17
BuildTools <g.spam32@gmail.com>
2016-11-24 10:25:15 +0100

    Skapade ColorMenu

---
+++ Spel/A-nivå/interface/scene/colormenu.py
@@ -0,0 +1,11 @@
+from interface.scene import Scene
+import interface.assets as assets
+from interface.component.button import Button
+
+import sfml as sf
+
+class ColorMenu(Scene):
+    """Class representing menu that lets the player
+    choose the color to play as."""
+
+    pass
